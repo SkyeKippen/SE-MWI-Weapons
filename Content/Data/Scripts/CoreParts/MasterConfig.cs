95b01cbac9baa2d36a0080fefa667f024581c747
SkyeMeadows <skyemeadows20@gmail.com>
2026-09-18 01:54:15 -0500

    Initial Commit

---
+++ Content/Data/Scripts/CoreParts/MasterConfig.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Scripts
+{
+    partial class Parts
+    {
+        internal Parts()
+        {
+            // naming convention: WeaponDefinition Name
+            //
+            // Enable your definitions using the follow syntax:
+            // PartDefinitions(Your1stDefinition, Your2ndDefinition, Your3rdDefinition);
+            // PartDefinitions includes both weapons and phantoms
+            PartDefinitions(
+                MWI_TEC_01_Mk1,
+                MWI_SKC_02_Mk1,
+                MWI_TEL_03_Mk1
+            );
+
+
+        }
+    }
+}
