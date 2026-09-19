a7db11b585df3cc1b94ae52b42f14358f543117b
SkyeMeadows <skyemeadows20@gmail.com>
2026-09-18 23:07:53 -0500

    Adjustments to MEL_04_Mk1 Firing Audio

--- Content/Data/Scripts/CoreParts/MWI/MWI_MEL_04_Mk1_Weapon.cs
+++ Content/Data/Scripts/CoreParts/MWI/MWI_MEL_04_Mk1_Weapon.cs
@@ -208,13 +208,13 @@ namespace Scripts {
                 Audio = new HardPointAudioDef
                 {
                     PreFiringSound = "", // Audio for warmup effect.
-                    FiringSound = "MWI_Arc_TEC_01_Mk1_Shoot", // Audio for firing.
+                    FiringSound = "MWI_Arc_TEL_04_Mk1_Shoot", // Audio for firing.
                     FiringSoundPerShot = true, // Whether to replay the sound for each shot, or just loop over the entire track while firing.
                     ReloadSound = "", // Sound SubtypeID, for when your Weapon is in a reloading state
                     NoAmmoSound = "", // Sound for if the user attempts to fire the gun without ammo
                     HardPointRotationSound = "WepTurretGatlingRotate", // Audio played when turret is moving.
                     BarrelRotationSound = "", // Sound played when the barrel rotates
-                    FireSoundEndDelay = 120, // How long the firing audio should keep playing after firing stops. Measured in game ticks(6 = 100ms, 60 = 1 seconds, etc..).
+                    FireSoundEndDelay = 0, // How long the firing audio should keep playing after firing stops. Measured in game ticks(6 = 100ms, 60 = 1 seconds, etc..).
                     FireSoundNoBurst = true, // Don't stop firing sound from looping when delaying after burst.
                 },
                 Graphics = new HardPointParticleDef
